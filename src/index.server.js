fc5772d9ec2fb0fafe1e2fda8f2de26f8eb804c4
Sm-Ashraful <smashraful.dev@gmail.com>
2023-12-22 00:47:24 +0600

    utils code update

--- src/index.server.js
+++ src/index.server.js
@@ -4,4 +4,15 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(`Server Crashed due to ${err.message}`);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Mongo db connection Failed ${err.message}`);
+  });

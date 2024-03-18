822e98c1f5da18f3c91f813f651d7ee715810d7b
Sm-Ashraful <smashraful.dev@gmail.com>
2024-03-18 16:21:34 +0600

    suer controller, product route changing

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,14 +3,19 @@ import { upload } from "../middlewares/multer.middleware.js";
 import {
   createProduct,
   getAllProducts,
+  getProductById,
 } from "../controllers/product.controller.js";
 import { createProductValidator } from "../validators/product.validators.js";
 import { validate } from "../validators/validate.js";
 import { verifyAdmin } from "../middlewares/admin.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { mongoIdPathVariableValidator } from "../validators/mongodb.validators.js";
 
 const router = Router();
 router.route("/").get(getAllProducts);
+router
+  .route("/:productId")
+  .get(mongoIdPathVariableValidator("productId"), validate, getProductById);
 router.route("/create-product").post(
   upload.fields([
     {
